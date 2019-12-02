236701dd18a640d528dbca97a658354532ee9e4b
Cemal Gunes <cemal.gunes@logo.com.tr>
2019-12-02 13:31:49 +0300

    initial commit

---
+++ json_http/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:json_http/remote_api.dart';
+
+import 'local_json.dart';
+
+void main() => runApp(MyApp());
+
+/*
+class Ogrenci{
+  int id;
+  String isim;
+  Ogrenci(this.id, this.isim);
+  @override
+  String toString() {
+    return "Adı : $isim id: $id";
+  }
+  factory Ogrenci.mapiNesneyeDonustur(Map<String, dynamic> gelenMap){
+    return Ogrenci(gelenMap['id'], gelenMap['isim']);
+  }
+  Ogrenci.fromMap(Map<String, dynamic> gelenMap):
+      id = gelenMap['id'],
+      isim = gelenMap['isim'];
+}
+*/
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+
+  @override
+  Widget build(BuildContext context) {
+
+/*
+    Ogrenci emre=Ogrenci(10, "emre");
+    debugPrint(emre.toString());
+    Map<String, dynamic> hasanMap={'id' : 15, 'isim' : "Hasan"};
+    debugPrint("Adı : "+hasanMap['isim'] + " id: "+hasanMap['id'].toString());
+    Ogrenci yeni=Ogrenci.mapiNesneyeDonustur(hasanMap);
+    debugPrint(yeni.toString());
+    Map<String, dynamic> fatmaMap={'id' : 20, 'isim' : "Fatma"};
+    Ogrenci fatma=Ogrenci.fromMap(fatmaMap);
+    debugPrint(fatma.toString());
+*/
+
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Json ve Api"),),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(child:Text("Local Json"), color: Colors.green, onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LocalJsonKullanimi()));
+            },),
+            RaisedButton(child:Text("Remote Api"), color: Colors.orangeAccent, onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>RemoteApiKullanimi()));
+            },),
+          ],
+        ),
+      ),
+    );
+  }
+}
